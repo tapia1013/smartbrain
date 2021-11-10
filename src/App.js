f5bef4a4582bd347764a239c857a34bf3f47f222
tapia1013 <manbearpig1013@gmail.com>
2021-11-09 18:32:40 -0800

    outdated particles and api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react'
+// import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 import './App.css';
 
 
 
 
+class App extends React.Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/*
-      
-      <ImageLinkForm />
-      <FaceRecognition />*/}
-    </div>
-  );
+
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  }
+
+
+  onButtonSubmit = () => {
+
+    app.models.predict("08437eed2f7c44b4951d0480cc5a7246", "https://samples.clarifai.com/face-det.jpg").then(
+      function (response) {
+
+      },
+      function (err) {
+
+      }
+    )
+
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-reportWebVitals();
